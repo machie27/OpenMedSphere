987def9c713ada3fc0476510dc5dd87fa55aeb95
Stephen <machie27@hotmail.com>
2025-11-01 14:38:36 +0100

    need to fix endpoints

--- src/Presentation/OpenMedSphere.AppHost/Program.cs
+++ src/Presentation/OpenMedSphere.AppHost/Program.cs
@@ -1,5 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.OpenMedSphere_API>("openmedsphere-api");
+var api = builder.AddProject<Projects.OpenMedSphere_API>("openmedsphere-api")
+    .WithHttpHealthCheck("/health");
+
+builder.AddProject<Projects.OpenMedSphere_Web>("openmedsphere-frontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(api)
+    .WaitFor(api);
+
 
 builder.Build().Run();
